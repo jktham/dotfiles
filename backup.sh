9898b17612162b87205094894ac0c99b68231cc2
jktham-sf <jonas.tham@gmx.ch>
2025-03-12 15:10:58 +0100

    clean up backup script, update

--- backup.sh
+++ backup.sh
@@ -1,50 +1,40 @@
-#!bin/bash
+#!/bin/bash
 
-mkdir -p ~/stuff/dotfiles/$1
-mkdir -p ~/stuff/dotfiles/$1/.config
-mkdir -p ~/stuff/dotfiles/$1/.config/bspwm
-mkdir -p ~/stuff/dotfiles/$1/.config/sxhkd
-mkdir -p ~/stuff/dotfiles/$1/.config/picom
-mkdir -p ~/stuff/dotfiles/$1/.config/polybar
-mkdir -p ~/stuff/dotfiles/$1/.config/kitty
-mkdir -p ~/stuff/dotfiles/$1/.config/kitty/themes
-mkdir -p ~/stuff/dotfiles/$1/.config/bg
-mkdir -p ~/stuff/dotfiles/$1/.config/neofetch
-mkdir -p ~/stuff/dotfiles/$1/.config/rofi
-mkdir -p ~/stuff/dotfiles/$1/.config/spotifyd
-mkdir -p ~/stuff/dotfiles/$1/.config/dunst
-mkdir -p ~/stuff/dotfiles/$1/.config/mpv
+# dirs to back up, relative to ~/
+declare -a dirs_home=(
+	".fehbg"
+	".xinitrc"
+	".Xresources"
+	".zprofile"
+	".zaliases"
+	".zshrc"
+	".xinputrc"
+)
 
-cp ~/.fehbg ~/stuff/dotfiles/$1
-cp ~/.xinitrc ~/stuff/dotfiles/$1
-cp ~/.Xresources ~/stuff/dotfiles/$1
-cp ~/.zprofile ~/stuff/dotfiles/$1
-cp ~/.zaliases ~/stuff/dotfiles/$1
-cp ~/.zshrc ~/stuff/dotfiles/$1
+declare -a dirs_config=(
+	".config/bspwm" 
+	".config/sxhkd" 
+	".config/picom"
+	".config/polybar"
+	".config/kitty"
+	".config/bg"
+	".config/neofetch"
+	".config/rofi"
+	".config/spotifyd"
+	".config/dunst"
+	".config/mpv"
+	".config/nvim"
+)
 
-cp ~/.config/bspwm/bspwmrc ~/stuff/dotfiles/$1/.config/bspwm
-cp ~/.config/sxhkd/sxhkdrc ~/stuff/dotfiles/$1/.config/sxhkd
-cp ~/.config/picom/picom.conf ~/stuff/dotfiles/$1/.config/picom
+mkdir -p ./$1
+for d in "${dirs_home[@]}"; do
+   cp -r ~/$d ./$1
+done
 
-cp ~/.config/polybar/config.ini ~/stuff/dotfiles/$1/.config/polybar
-cp ~/.config/polybar/launch.sh ~/stuff/dotfiles/$1/.config/polybar
+mkdir -p ./$1/.config
+for d in "${dirs_config[@]}"; do
+   cp -r ~/$d ./$1/.config
+done
 
-cp ~/.config/kitty/themes/* ~/stuff/dotfiles/$1/.config/kitty/themes
-cp ~/.config/kitty/current-theme.conf ~/stuff/dotfiles/$1/.config/kitty
-cp ~/.config/kitty/kitty.conf ~/stuff/dotfiles/$1/.config/kitty
-
-cp ~/.config/bg/firewatch-day.jpg ~/stuff/dotfiles/$1/.config/bg
-cp ~/.config/bg/firewatch-night.jpg ~/stuff/dotfiles/$1/.config/bg
-
-cp ~/.config/neofetch/config.conf ~/stuff/dotfiles/$1/.config/neofetch
-
-cp ~/.config/rofi/config.rasi ~/stuff/dotfiles/$1/.config/rofi
-cp ~/.config/rofi/list-aliases.sh ~/stuff/dotfiles/$1/.config/rofi
-cp ~/.config/rofi/theme.rasi ~/stuff/dotfiles/$1/.config/rofi
-
-cp ~/.config/spotifyd/spotifyd.conf ~/stuff/dotfiles/$1/.config/spotifyd
-sed -i ~/stuff/dotfiles/$1/.config/spotifyd/spotifyd.conf -e "s|username = \"[^@]*\"|username = \"\"|"
-sed -i ~/stuff/dotfiles/$1/.config/spotifyd/spotifyd.conf -e "s|password = \"[^@]*\"|password = \"\"|"
-
-cp -r ~/.config/dunst ~/stuff/dotfiles/$1/.config
-cp -r ~/.config/mpv ~/stuff/dotfiles/$1/.config/
+sed -i ./$1/.config/spotifyd/spotifyd.conf -e "s|username = \"[^@]*\"|username = \"\"|"
+sed -i ./$1/.config/spotifyd/spotifyd.conf -e "s|password = \"[^@]*\"|password = \"\"|"
